7ceebd4ee44af0f6a998b45090f3c238c8a1773c
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-03 16:25:18 -0300

    Privado e protegido

--- Conta.js
+++ Conta.js
@@ -22,10 +22,18 @@ export class Conta{
     }
 
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa) {
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
+
+        return 0;
     }
 
     depositar(valor){
