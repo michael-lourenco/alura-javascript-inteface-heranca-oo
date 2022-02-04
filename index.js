b2d57602d12aae9a8b4ed9df1342295559023ec2
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-03 23:33:54 -0300

    Interfaces

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const diretor = new Diretor("Rodrigo", 10000, "123.456.789-10");
 diretor.cadastrarSenha("123456799");
 const gerente = new Gerente("João", 5000, "987.654.321-10");
 gerente.cadastrarSenha("123456789");
-const estaLogado = SistemaAutenticacao.login(gerente, "123456789");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123456789");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456799");
 
-console.log(estaLogado)
+const cliente = new Cliente("João", "123.456.789-10", "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123");
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
