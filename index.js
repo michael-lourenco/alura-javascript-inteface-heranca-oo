f585cc7047c4e9f19186051cec9c73c1b3c4a5fb
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-03 23:22:08 -0300

    Polimorfismo

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-import { Cliente } from "./Cliente.js"
-import { ContaCorrente } from "./Conta/ContaCorrente.js"
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, "123.456.789-10");
+diretor.cadastrarSenha("123456799");
+const gerente = new Gerente("João", 5000, "987.654.321-10");
+gerente.cadastrarSenha("123456789");
+const estaLogado = SistemaAutenticacao.login(gerente, "123456789");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1,1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
-
-const contaPoupanca = new ContaPoupanca(500, cliente1, 1001);
-contaPoupanca.sacar(50);
-console.log(contaPoupanca)
-console.log(contaCorrenteRicardo);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(1000);
-contaSalario.sacar(100);
-
-console.log(contaSalario);
+console.log(estaLogado)
